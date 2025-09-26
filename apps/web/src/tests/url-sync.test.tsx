a1dc43f96870b893a7c705c9c9350d6cec01ae7e
kazyel <m.mascarelo@gmail.com>
2025-09-26 00:00:55 -0300

    tests: added tests for all cases of url tag syncing

--- apps/web/src/tests/url-sync.test.tsx
+++ apps/web/src/tests/url-sync.test.tsx
@@ -7,8 +7,8 @@ type TagActions = "add" | "remove" | "filter";
 test("url sync is applying filters correctly", () => {
   const oldURL = {
     page: undefined,
-    year: undefined,
     tags: undefined,
+    year: undefined,
   } as const;
 
   const searchProps = {
@@ -23,6 +23,73 @@ test("url sync is applying filters correctly", () => {
   expect(nextResult).toStrictEqual({
     page: undefined,
     tags: "coding",
+    year: undefined,
+  });
+});
+
+test("url sync is aggregating filters correctly", () => {
+  const oldURL = {
+    page: undefined,
+    tags: "coding",
+    year: undefined,
+  } as const;
+
+  const searchProps = {
+    tagOptions: {
+      action: "add" as TagActions,
+      tag: "thoughts" as Tags[number],
+    },
+  };
+
+  const nextResult = oldURLParser(oldURL, searchProps);
+
+  expect(nextResult).toStrictEqual({
+    page: undefined,
+    tags: "coding,thoughts",
+    year: undefined,
+  });
+});
+
+test("url sync is filtering filters correctly", () => {
+  const oldURL = {
+    page: undefined,
+    tags: "coding,thoughts",
+    year: undefined,
+  } as const;
+
+  const searchProps = {
+    tagOptions: {
+      action: "filter" as TagActions,
+      tag: "thoughts" as Tags[number],
+    },
+  };
+
+  const nextResult = oldURLParser(oldURL, searchProps);
+
+  expect(nextResult).toStrictEqual({
+    page: undefined,
+    tags: "coding",
+    year: undefined,
+  });
+});
+
+test("url sync is removing filters entirely", () => {
+  const oldURL = {
+    page: undefined,
+    tags: "coding,thoughts",
+    year: undefined,
+  } as const;
+
+  const searchProps = {
+    tagOptions: {
+      action: "remove" as TagActions,
+    },
+  };
+
+  const nextResult = oldURLParser(oldURL, searchProps);
+
+  expect(nextResult).toStrictEqual({
+    page: undefined,
     year: undefined,
   });
 });
